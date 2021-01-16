a756c03b0bf4e767795568b1099fea57ee30e4ab
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-01-16 13:49:54 +0200

    query and mutation - just for example

--- server/resolvers/post.js
+++ server/resolvers/post.js
@@ -1,6 +1,24 @@
-const totalPosts = () => 52;
+const { posts } = require("../temp.js");
+
+const totalPosts = () => posts.length;
+const allPosts = () => posts;
+const newPost = (parent, args, context) => {
+  //create new post object
+  const post = {
+    id: posts.length + 1,
+    title: args.title,
+    description: args.description,
+  };
+  posts.push(post);
+  return post;
+};
+
 module.exports = {
   Query: {
     totalPosts,
+    allPosts,
+  },
+  Mutation: {
+    newPost,
   },
 };
